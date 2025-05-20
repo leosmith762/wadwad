0cde66996331f6648680ce776d797f1ae964e652
leosmith762 <yousuck696969699@outlook.com>
2025-05-20 17:46:20 +0100

    Delete utils directory

--- utils/utils.js
+++
@@ -1,9 +0,0 @@
-function getUptime() {
-  const totalSeconds = process.uptime();
-  const hours = Math.floor(totalSeconds / 3600);
-  const minutes = Math.floor(totalSeconds % 3600 / 60);
-  const seconds = Math.floor(totalSeconds % 60);
-  return `${hours}h ${minutes}m ${seconds}s`;
-}
-
-module.exports = { getUptime };
